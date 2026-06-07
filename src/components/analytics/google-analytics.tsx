78b331928d2d84c0b6ccdc667a833d257a883b2f
Ditto Cloner <cloner@ditto.local>
2026-06-07 11:11:44 +0100

    feat(analytics): switch to GTM container GT-NBBTLQK2

--- src/components/analytics/google-analytics.tsx
+++ src/components/analytics/google-analytics.tsx
@@ -4,40 +4,37 @@ import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 import { useEffect, Suspense } from "react";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GTM_ID = "GT-NBBTLQK2";
 
 function GoogleAnalyticsPageview() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (!GA_ID) return;
     const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : "");
-    window.gtag?.("config", GA_ID, { page_path: url });
+    window.gtag?.("config", GTM_ID, { page_path: url });
   }, [pathname, searchParams]);
 
   return null;
 }
 
 export function GoogleAnalytics() {
-  if (!GA_ID) return null;
-
   return (
     <>
       <Script
-        id="ga-loader"
+        id="gtag-loader"
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
       />
       <Script
-        id="ga-init"
+        id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_ID}', { send_page_view: false });
+            gtag('config', '${GTM_ID}');
           `,
         }}
       />
